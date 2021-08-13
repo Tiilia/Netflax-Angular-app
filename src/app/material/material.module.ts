05b877bceb132029bf37c1fd3efbda4d7fad296f
Tiilia <dosimontp@gmail.com>
2021-08-13 20:12:05 +0200

    add card

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,13 +3,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 
 const MaterialComponents = [
   // import modules material
   MatButtonModule,
   MatButtonToggleModule,
   MatPaginatorModule,
-  MatToolbarModule
+  MatToolbarModule,
+  MatCardModule,
 ];
 
 @NgModule({
